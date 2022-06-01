583838ffd402bb75b224ca59a2d9217ab5c10d58
zoz0312 <zoz0312@naver.com>
2022-06-01 19:06:52 +0900

    react createElement Function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// const element = <h1 title="foo">Hello</h1>
-// const container = document.getElementById("root")
-// ReactDOM.render(element, container)
-
 const element = {
   type: 'h1',
   props: {
