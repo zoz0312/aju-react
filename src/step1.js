666c67ff5cee0e4107337dfd013774e500bbf66f
zoz0312 <zoz0312@naver.com>
2022-06-01 19:13:07 +0900

    3. createTextElement function

--- src/step1.js
+++ src/step1.js
@@ -3,7 +3,19 @@ function createElemnt(type, props, ...children) {
     type,
     props: {
       ...props,
-      children,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child)
+      ),
+    },
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
     },
   }
 }
