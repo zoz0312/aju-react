7d370f54c0cfb19bd3616600fc6f03d264bc3fd1
zoz0312 <zoz0312@naver.com>
2022-06-06 21:44:30 +0900

    10. dom event listeners add & remove

--- src/aju.js
+++ src/aju.js
@@ -36,6 +36,15 @@ const isProperty = (key) => key !== 'children' && !isEvent(key)
 const isNew = (prev, next) => (key) => prev[key] !== next[key]
 const isGone = (prev, next) => (key) => !(key in next)
 function updateDom(dom, prevProps, nextProps) {
+  //Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.removeEventListener(eventType, prevProps[name])
+    })
+
   // Remove old properties
   Object.keys(prevProps)
     .filter(isProperty)
@@ -50,6 +59,15 @@ function updateDom(dom, prevProps, nextProps) {
     .filter(isNew(prevProps, nextProps))
     .forEach((name) => {
       dom[name] = nextProps[name]
+    })
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.addEventListener(eventType, nextProps[name])
     })
 }
 
